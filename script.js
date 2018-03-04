1ff8ee7964d2a798c4814a6238dd57ab55be2e19
alisonstardust <zakulisami@o2.pl>
2018-03-04 08:13:34 +0100

    validation updated

--- script.js
+++ script.js
@@ -3,40 +3,20 @@ const formName = document.querySelector('.website_formName');
 const formSurname = document.querySelector('.website_formSurname');
 const formEmail = document.querySelector('.website_formEmail');
 const alert = document.querySelectorAll('.website_form--alert');
-console.log(alert[0])
+console.log(alert)
 
 
 form.addEventListener('submit', function() {
-  validateName();
-  validateSurname();
-  validateEmail();
+  validate((formName.value.length < 1 || formName.value.match(/\d|\s|[.*+?^${}@()|[\]\\]/g)), alert[0]);
+  validate((formSurname.value.length < 1 || formSurname.value.match(/\d/)), alert[1]);
+  validate(!(formEmail.value.match(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/)), alert[2]);
 });
 
-
-function validateName() {
-  if (formName.value.length < 1 || formName.value.match(/\d|\s/g)) {
-    alert[0].style.display = "block";
+function validate(a, b) {
+  if (a) {
+    b.style.display = "block";
   } else {
-    alert[0].style.display = "none";
-    console.log(formName.value);
-  }
-}
-
-function validateSurname() {
-  if (formSurname.value.length < 1 || formSurname.value.match(/\d/)) {
-    alert[1].style.display = "block";
-  } else {
-    alert[1].style.display = "none";
-    console.log(formSurname.value);
-  }
-}
-
-function validateEmail() {
-  if (formEmail.value.match(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/)) {
-    alert[2].style.display = "none";
-    console.log(formEmail.value);
-  }
-  else {
-    alert[2].style.display = "block";
+    b.style.display = "none";
+    console.log(formName.value, formSurname.value, formEmail.value);
   }
 }
