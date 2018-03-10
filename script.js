31a7a8c26ae9ce72f96604503eb4539c03bded43
alisonstardust <zakulisami@o2.pl>
2018-03-10 16:45:27 +0100

    form updated

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const alert = document.querySelectorAll('.website_form--alert');
 //console.log(alert)
 
 
-form.addEventListener('submit', function() {
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
   validate((formName.value.length < 1 || formName.value.match(/\d|\s|[.*+?^${}@()|[\]\\]/g)), alert[0]);
   validate((formSurname.value.length < 1 || formSurname.value.match(/\d/)), alert[1]);
   validate(!(formEmail.value.match(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/)), alert[2]);
