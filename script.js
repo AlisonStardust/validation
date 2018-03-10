f5bd4472fb95f3706e4f088d0784eeda1ccc3e7d
alisonstardust <zakulisami@o2.pl>
2018-03-10 09:28:39 +0100

    phone number validation

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ const form = document.querySelector('.website_form');
 const formName = document.querySelector('.website_formName');
 const formSurname = document.querySelector('.website_formSurname');
 const formEmail = document.querySelector('.website_formEmail');
+const formPhone = document.querySelector('.website_formPhone');
+const formSent = document.querySelector('.website_form--correct');
 const alert = document.querySelectorAll('.website_form--alert');
-console.log(alert)
+//console.log(alert)
 
 
 form.addEventListener('submit', function() {
   validate((formName.value.length < 1 || formName.value.match(/\d|\s|[.*+?^${}@()|[\]\\]/g)), alert[0]);
   validate((formSurname.value.length < 1 || formSurname.value.match(/\d/)), alert[1]);
   validate(!(formEmail.value.match(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/)), alert[2]);
+  validate(!(formPhone.value.match(/\d/g)), alert[3]);
+  showCorrect();
 });
 
 function validate(condition, alertSelector) {
@@ -17,6 +21,15 @@ function validate(condition, alertSelector) {
     alertSelector.style.display = "block";
   } else {
     alertSelector.style.display = "none";
-    console.log(formName.value, formSurname.value, formEmail.value);
+    let submittedPhone = formPhone.value.split(" ").join("");
+    console.log(formName.value, formSurname.value, formEmail.value, submittedPhone);
+  }
+}
+
+function showCorrect() {
+  if ((alert[0].style.display === "none") && (alert[1].style.display === "none") && (alert[2].style.display === "none") && (alert[3].style.display === "none")) {
+    formSent.style.display = "block";
+  } else {
+    console.log("incorrect form");
   }
 }
